54244ddd7489041f4d7bcd75dccf7c94e6f5f5dc
chetna.xnava@gmail.com <chetna.xnava@gmail.com>
2026-03-22 18:31:49 +0530

    db change

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, // Neon requires SSL
   },
-  family: 4,              // <-- Force IPv4
+  family: 4,                // Force IPv4
   connectionTimeoutMillis: 10000,
 });
 
